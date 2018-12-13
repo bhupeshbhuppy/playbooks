26c47bc8e05aee1c4059c386aff2c1c46c49df63
bhupeshbhuppy <bhupesh.bhuppy@gmail.com>
2018-12-14 00:06:23 +0530

    Added discriminatory tests

--- logistic_regression.R
+++ logistic_regression.R
@@ -11,7 +11,9 @@ library(plotly)
 library(DescTools)
 library(reshape2)
 library(perturb)
-
+library(ROCR)
+library(Metrics)
+library(Hmisc)
 # user defined functions --------------------------------------------------
 
 woe<-function(dep,indep){
@@ -158,9 +160,84 @@ if(length(model)>0){
       temp_model<-glm(glm(dep~., modelling_data[,-c(i+1)],family=binomial(link = "logit")))
       print(paste("After reoving the ",i,"th varible the model VIF is", VIF(temp_model)))
       
-      #### Need to check the PM after they are all writen
+      #### Need to check the PM after they are all written
     }
   }
+  
+  # 3. DIscritiminatory Power Testing -------------------------------------------
+  #########################################################################################################
+  ####                                    a. ROC/ AUC                                                 #####
+  #########################################################################################################
+  
+  AUC<-round(auc(dep, predict(model)),3)
+  rocs <- performance(prediction(predict(model), dep), "tpr", "fpr")
+  ROC_df<-data.frame("fpr"=rocs@x.values,"tpr"=rocs@y.values)
+  colnames(ROC_df)<-c("fpr","tpr")
+  
+  plot_ly(ROC_df, y = ~tpr, x = ~fpr) %>% 
+    
+    add_lines(name = "Model",
+              line = list(shape = "spline", color = "#737373", width = 7), 
+              fill = "tozeroy", fillcolor = "#2A3356") %>% 
+    
+    
+    add_segments(x = 0, y = 0, xend = 1, yend = 1, 
+                 line = list(dash = "7px", color = "#F35B25", width = 4), 
+                 name = "Random") %>% 
+    
+    add_segments(x = 0, y = 0, xend = 0, yend = 1, 
+                 line = list(dash = "10px", color = "black", width = 4), 
+                 showlegend = F) %>%
+    
+    add_segments(x = 0, y = 1, xend = 1, yend = 1, 
+                 line = list(dash = "10px", color = "black", width = 4), 
+                 showlegend = F) %>% 
+    
+    add_annotations(x = 0.8, y = 0.2, showarrow = F, 
+                    text = paste0("Area Under Curve: ", AUC),
+                    font = list(family = "serif", size = 18, color = "#E8E2E2")) %>%
+    
+    add_annotations(x = 0, y = 1, showarrow = F, xanchor = "left", 
+                    xref = "paper", yref = "paper",
+                    text = paste0("Receiver Operator Curve"),
+                    font = list(family = "arial", size = 30, color = "#595959")) %>%
+    
+    add_annotations(x = 0, y = 0.95, showarrow = F, xanchor = "left", 
+                    xref = "paper", yref = "paper",
+                    text = paste0("Charts the percentage of correctly identified defaults (hits) against the percentage of non defaults incorrectly identifed as defaults (false alarms)"),
+                    font = list(family = "serif", size = 14, color = "#999999")) %>% 
+    
+    
+    layout(xaxis = list(range = c(0,1), zeroline = F, showgrid = F,
+                        title = "Number of False Alarms"),
+           yaxis = list(range = c(0,1), zeroline = F, showgrid = F,
+                        domain = c(0, 0.9),
+                        title = "Number of Hits"),
+           plot_bgcolor = "#E8E2E2",
+           height = 800, width = 1024)
+  
+  #########################################################################################################
+  ####                                    b. SomersD'                                                 #####
+  ####  It is calculated as the diference between the concordant and discordant pairs.                #####
+  ####  SomersD'(Dxy) = (NCon - NDis)/(NCon + NDis + NTie). It is related to ROC/AUC as Dxy=2(AUC-0.5)##### 
+  ####  A vale close to 0 indicates a random model. A lower value does not always mean that the model #####
+  ####  should be rejected.                                                                           #####
+  #########################################################################################################
+  print(paste("Somers' D is: ",2*(AUC-0.5)))
+
+  #########################################################################################################
+  ####                                    C. Kolmogrov Smirnov(KS)                                    #####
+  ####  It is used to test for difference between distribution function thereforecan be used to       #####
+  ####  discriminate between the two binary responses. Higher value mean higher discriminatory power  ##### 
+  ####  KS is not sensitive to the shape of distribution and should be complemented by other tests    #####
+  ####  KS = Maximum difference between Cumulative % 1 and Cumulative % 0  along different Quartile   #####
+  ####  https://www.listendata.com/2015/01/model-validation-in-logistic-regression.html               #####
+  #########################################################################################################
+  diff_TPRFPR=ROC_df$tpr-ROC_df$fpr 
+  KS=max(diff_TPRFPR)
+  cutoffAtKS=unlist(rocs@alpha.values)[which.max(diff_TPRFPR)]
+  print(paste("The KS and corresponding cutoff is: ",c(KS, cutoffAtKS)))    
+  
 }else{
   print("Please create or import the model")
 }
